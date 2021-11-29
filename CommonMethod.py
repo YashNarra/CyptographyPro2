6d59e9ef1e2cf054585fa6510a83da9e2782b581
Renting <trttong@outlook.com>
2021-11-28 21:58:40 -0500

    update BS, SR

--- CommonMethod.py
+++ CommonMethod.py
@@ -7,3 +7,19 @@
 @description:
 Common method collection.
 """
+
+
+def group_input_text(text, size=4):
+    """
+    Convert 128 bits input to 16 bytes of 8 bits.
+    :param text: [str] input text.
+    :param size: [int] default value is 4.
+    :return: [2-D list] converted text.
+    """
+    text = text.split()
+    return [text[i:i+size] for i in range(0, len(text), size)]
+
+
+if __name__ == "__main__":
+    text = "00 11 22 33 44 55 66 77 88 99 aa bb cc dd ee ff"
+    print(group_input_text(text))
